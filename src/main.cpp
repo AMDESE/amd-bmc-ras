2f0412b4294a6bb8d4a3da4b48a40c0355e2f9db
Supreeth Venkatesh <supreeth.venkatesh@amd.com>
2022-02-10 20:44:24 -0600

    bmc-ras: Initial Commit
    Initial version that handles FATAL RAS errors.
    
    Signed-off-by: Abinaya <abinaya.dhandapani@amd.com>
    Signed-off-by: Supreeth Venkatesh <supreeth.venkatesh@amd.com>

---
+++ src/main.cpp
@@ -0,0 +1,353 @@
+#include <iostream>
+#include <gpiod.hpp>
+#include <boost/asio.hpp>
+#include <boost/date_time/posix_time/posix_time.hpp>
+#include <fstream>
+#include <boost/asio/io_service.hpp>
+#include <boost/asio/posix/stream_descriptor.hpp>
+#include <boost/asio/steady_timer.hpp>
+#include <boost/container/flat_map.hpp>
+#include <boost/container/flat_set.hpp>
+#include <boost/asio.hpp>
+#include <boost/asio/spawn.hpp>
+#include <boost/asio/error.hpp>
+#include <sdbusplus/asio/connection.hpp>
+#include <sdbusplus/asio/property.hpp>
+#include <phosphor-logging/log.hpp>
+#include <sdbusplus/asio/object_server.hpp>
+
+extern "C" {
+#include <sys/stat.h>
+#include "linux/i2c-dev.h"
+#include "i2c/smbus.h"
+#include "esmi_common.h"
+#include "esmi_mailbox.h"
+#include "esmi_rmi.h"
+}
+
+#define COMMAND_BOARD_ID    ("/sbin/fw_printenv -n board_id")
+#define COMMAND_LEN         3
+
+#define SUCCESS  1
+#define FAILURE  0
+
+static boost::asio::io_service io;
+std::shared_ptr<sdbusplus::asio::connection> conn;
+
+static std::string BoardName;
+static uint32_t err_count = 0;
+
+static gpiod::line P0_apmlAlertLine;
+static boost::asio::posix::stream_descriptor P0_apmlAlertEvent(io);
+
+static gpiod::line P1_apmlAlertLine;
+static boost::asio::posix::stream_descriptor P1_apmlAlertEvent(io);
+
+struct i2c_info p0_info = {2, 60};
+struct i2c_info p1_info = {3, 56};
+
+const static constexpr int resetPulseTimeMs = 500;
+static boost::asio::steady_timer gpioAssertTimer(io);
+
+bool harvest_ras_errors(struct i2c_info info)
+{
+    uint16_t n = 0;
+    uint16_t bytespermca;
+    uint16_t numbanks = 0;
+    uint32_t buffer;
+    struct mca_bank mca_dump;
+    oob_status_t ret;
+	std::string file;
+    std::ofstream outfile;
+
+    std::cerr << "read_bmc_ras_mca_validity_check\n";
+    ret = read_bmc_ras_mca_validity_check(info, &bytespermca, &numbanks);
+    if (ret != OOB_SUCCESS) {
+            printf("Failed to get MCA banks with valid status Err[%d]\n",ret);
+		return false;
+    }
+	std::cout << " numbanks = " << numbanks << " bytespermca = " << bytespermca << std::endl;
+
+    if(numbanks > 0 ) {
+        phosphor::logging::log<phosphor::logging::level::INFO>
+            ("MCA banks returned non zero value.Harvesting RAS errors");
+
+		std::cout << "MCA valid banks = " << numbanks << std::endl;
+
+        file = "/var/lib/amd-ras/ras-error" + std::to_string(err_count) + ".txt";
+        err_count++;
+
+		outfile.open(file, std::ios_base::app);
+        while(n < numbanks) {
+            ret = read_bmc_ras_mca_msr_dump(info, mca_dump, &buffer);
+            if (ret != OOB_SUCCESS) {
+                printf("Failed to get MCA bank data, Err[%d]\n",ret);
+				n++;
+                continue;
+            }
+			outfile << buffer;
+            n++;
+        }
+		outfile.close();
+    }
+	return true;
+}
+
+bool getPlatformID()
+{
+    FILE *pf;
+    char data[COMMAND_LEN];
+
+    // Setup pipe for reading and execute to get u-boot environment
+    // variable board_id.
+    pf = popen(COMMAND_BOARD_ID,"r");
+
+    // Error handling
+    if(pf < 0)
+    {
+        std::cerr << "Unable to get Board ID, errno: " << errno << "message: " << strerror(errno) << "\n";
+        return false;
+    }
+
+    // Get the data from the process execution
+    if (fgets(data, COMMAND_LEN, pf) == NULL)
+    {
+        std::cerr << "Board ID data is null, errno: " << errno << "message: " << strerror(errno) << "\n";
+        return false;
+    }
+
+    // the data is now in 'data'
+    if (pclose(pf) != 0)
+    {
+        std::cerr << " Error: Failed to close command stream\n";
+        return false;
+    }
+    std::string board_id(data);
+    if((board_id.compare("3D") == 0) || (board_id.compare("40") == 0) || (board_id.compare("41") == 0)
+        || (board_id.compare("42") == 0) || (board_id.compare("52") == 0))
+    {
+        BoardName = "Onyx";
+        return true;
+    }
+    if((board_id.compare("3E") == 0 ) || (board_id.compare("43") == 0) || (board_id.compare("44") ==0)
+        || (board_id.compare("45") == 0) || (board_id.compare("51") == 0))
+    {
+        BoardName = "Quartz";
+        return true;
+    }
+    if((board_id.compare("46")== 0) || (board_id.compare("47") == 0) || (board_id.compare("48") == 0))
+    {
+        BoardName = "Ruby";
+        return true;
+    }
+    if((board_id.compare("49") == 0 ) || (board_id.compare("4A") == 0) || (board_id.compare("4B") == 0)
+        || (board_id.compare("4C") == 0) || (board_id.compare("4D") == 0) || (board_id.compare("4E") == 0))
+    {
+        BoardName = "Titanite";
+        return true;
+    }
+
+    return false;
+}
+
+static bool requestGPIOEvents(
+    const std::string& name, const std::function<void()>& handler,
+    gpiod::line& gpioLine,
+    boost::asio::posix::stream_descriptor& gpioEventDescriptor)
+{
+    // Find the GPIO line
+    gpioLine = gpiod::find_line(name);
+    if (!gpioLine)
+    {
+        std::cerr << "Failed to find the " << name << " line\n";
+        return false;
+    }
+
+    try
+    {
+        gpioLine.request(
+            {"RAS", gpiod::line_request::EVENT_BOTH_EDGES});
+    }
+    catch (std::exception& exc)
+    {
+        std::cerr << "Failed to request events for " << name << exc.what() << "\n";
+        return false;
+    }
+
+    int gpioLineFd = gpioLine.event_get_fd();
+    if (gpioLineFd < 0)
+    {
+        std::cerr << "Failed to get " << name << " fd\n";
+        return false;
+    }
+
+    gpioEventDescriptor.assign(gpioLineFd);
+
+    gpioEventDescriptor.async_wait(
+        boost::asio::posix::stream_descriptor::wait_read,
+        [&name, handler](const boost::system::error_code ec) {
+            if (ec)
+            {
+                std::cerr << name << " fd handler error: " << ec.message()
+                          << "\n";
+                // TODO: throw here to force power-control to restart?
+                return;
+            }
+            handler();
+        });
+
+    return true;
+}
+
+static bool setGPIOOutput(const std::string& name, const int value,
+                          gpiod::line& gpioLine)
+{
+    // Find the GPIO line
+    gpioLine = gpiod::find_line(name);
+    if (!gpioLine)
+    {
+        std::cerr << "Failed to find the " << name << " line.\n";
+        return false;
+    }
+
+    try
+    {
+        gpioLine.request({__FUNCTION__, gpiod::line_request::DIRECTION_OUTPUT});
+    }
+    catch (std::system_error& exc)
+    {
+        std::cerr << "Error setting gpio as Output: " << name << exc.what() << "\n";
+    }
+
+    try
+    {
+        // Request GPIO output to specified value
+        gpioLine.set_value(value);
+    }
+    catch (std::exception& exc)
+    {
+        std::cerr << "Failed to set value for " << name << exc.what()<< "\n";
+        return false;
+    }
+
+
+    std::cerr << name << " set to " << std::to_string(value) << "\n";
+
+    return true;
+}
+
+static int setGPIOValue(const std::string& name, const int value,
+                              const int durationMs)
+{
+    // No mask set, so request and set the GPIO normally
+    gpiod::line gpioLine;
+    if (!setGPIOOutput(name, value, gpioLine))
+    {
+        return -1;
+    }
+    gpioAssertTimer.expires_after(std::chrono::milliseconds(durationMs));
+    gpioAssertTimer.async_wait(
+        [gpioLine, value, name](const boost::system::error_code ec) {
+            // Set the GPIO line back to the opposite value
+            gpioLine.set_value(!value);
+            std::cerr << name << " released\n";
+            if (ec)
+            {
+                // operation_aborted is expected if timer is canceled before
+                // completion.
+                if (ec != boost::asio::error::operation_aborted)
+                {
+                    std::cerr << name << " async_wait failed: " << ec.message()
+                              << "\n";
+                }
+            }
+        });
+    return 0;
+}
+
+static void P0_apmlAlertHandler()
+{
+    gpiod::line_event gpioLineEvent = P0_apmlAlertLine.event_read();
+
+    if (gpioLineEvent.event_type == gpiod::line_event::FALLING_EDGE)
+    {
+    	std::cerr << "P0 APML Alert received\n";
+		sleep(2);
+    	harvest_ras_errors(p0_info);
+		setGPIOValue("ASSERT_RST_BTN_L", 0, resetPulseTimeMs);
+		sleep(5);
+    }
+    P0_apmlAlertEvent.async_wait(
+        boost::asio::posix::stream_descriptor::wait_read,
+        [](const boost::system::error_code ec) {
+            if (ec)
+            {
+                std::cerr << "P0 APML alert handler error: "
+                          << ec.message() << "\n";
+                return;
+            }
+            P0_apmlAlertHandler();
+        });
+}
+
+static void P1_apmlAlertHandler()
+{
+    gpiod::line_event gpioLineEvent = P1_apmlAlertLine.event_read();
+
+    if (gpioLineEvent.event_type == gpiod::line_event::FALLING_EDGE)
+    {
+    	std::cerr << "P1 APML Alert received\n";
+    	harvest_ras_errors(p1_info);
+        setGPIOValue("ASSERT_RST_BTN_L", 0, resetPulseTimeMs);
+        sleep(5);
+    }
+    P1_apmlAlertEvent.async_wait(
+        boost::asio::posix::stream_descriptor::wait_read,
+        [](const boost::system::error_code ec) {
+            if (ec)
+            {
+                std::cerr << "P1 APML alert handler error: "
+                          << ec.message() << "\n";
+                return;
+            }
+            P1_apmlAlertHandler();
+        });
+}
+
+int main() {
+
+	int dir;
+	struct stat buffer;
+	std::string ras_dir = "/var/lib/amd-ras/";
+
+    if(getPlatformID() == false)
+    {
+        phosphor::logging::log<phosphor::logging::level::INFO>(
+            "Couldnt find the board id of the platform");
+        return false;
+    }
+
+    if (stat(ras_dir.c_str(), &buffer) != 0) {
+   		dir = mkdir("/var/lib/amd-ras",0777);
+
+    	if(dir) {
+        	phosphor::logging::log<phosphor::logging::level::INFO>
+            	("ras-errror-logging directory not created");
+		}
+    }
+
+    conn = std::make_shared<sdbusplus::asio::connection>(io);
+
+	requestGPIOEvents("P0_I3C_APML_ALERT_L", P0_apmlAlertHandler, P0_apmlAlertLine, P0_apmlAlertEvent);
+
+	if((BoardName.compare("Quartz") == 0 )  ||
+		 (BoardName.compare("Titanite") == 0 )){
+
+		requestGPIOEvents("P1_I3C_APML_ALERT_L", P1_apmlAlertHandler, P1_apmlAlertLine, P1_apmlAlertEvent);
+	}
+
+	io.run();
+
+    return 0;
+}
+
