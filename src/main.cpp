d1865c08e05537f131fb78010119d2d6142d256a
Abinaya <abinaya.dhandapani@amd.com>
2022-02-18 10:20:08 -0600

    Harvest crash data over APML when bit 2 is set
    Signed-off-by: Abinaya <abinaya.dhandapani@amd.com>

--- src/main.cpp
+++ src/main.cpp
@@ -31,7 +31,7 @@ extern "C" {
 #define SUCCESS  1
 #define FAILURE  0
 
-#define MAX_RETRIES 128
+#define MAX_RETRIES 10
 
 static boost::asio::io_service io;
 std::shared_ptr<sdbusplus::asio::connection> conn;
@@ -279,41 +279,20 @@ bool harvest_ras_errors(struct i2c_info info,std::string alert_name)
     FILE *file;
     std::string filePath;
     uint8_t buf;
+    bool status = FAILURE;
 
     std::cerr << "read_bmc_ras_mca_validity_check" << std::endl;
 
-    if(alert_name.compare("P0_ALERT")   == 0 )
-	{
-		if (read_sbrmi_ras_status(p0_info, &buf) == 0)
-		{
-
-			if ((buf & 0x04))
-			{
-				setGPIOValue("ASSERT_RST_BTN_L", 0, resetPulseTimeMs);
-				return true;
-			}
-		} else {
-	        return false;
-        }
-    }
-	else if(alert_name.compare("P1_ALERT")   == 0 )
-	{
-        if (read_sbrmi_ras_status(p1_info, &buf) == 0)
-        {
-
-            if ((buf & 0x04))
-			{
-                setGPIOValue("ASSERT_RST_BTN_L", 0, resetPulseTimeMs);
-                return true;
-			}
-        } else {
-                return false;
-        }
+    if (read_sbrmi_ras_status(info, &buf) != 0)
+    {
+        status = FAILURE;
+        goto exit_L;
     }
 
     if (!(buf & 0x1)) {
         std::cout << "The alert signaled is not due to a fatal error" << std::endl;
-        return false;
+        status = FAILURE;
+        goto exit_L;
     }
 
     while (ret != OOB_SUCCESS)
@@ -325,7 +304,8 @@ bool harvest_ras_errors(struct i2c_info info,std::string alert_name)
         if (retries > MAX_RETRIES)
         {
             std::cerr << "Failed to get MCA banks with valid status Error :" << ret << std::endl;
-            return false;
+            status = FAILURE;
+            goto exit_L;
         }
 
         if (numbanks == 0)
@@ -373,7 +353,18 @@ bool harvest_ras_errors(struct i2c_info info,std::string alert_name)
     }
     fclose(file);
 
-    setGPIOValue("ASSERT_WARM_RST_BTN_L", 0, resetPulseTimeMs);
+    if ((buf & 0x04))
+    {
+        setGPIOValue("ASSERT_RST_BTN_L", 0, resetPulseTimeMs);
+        status = SUCCESS;
+        goto exit_L;
+    }
+    else {
+        setGPIOValue("ASSERT_WARM_RST_BTN_L", 0, resetPulseTimeMs);
+        status = SUCCESS;
+    }
+
+exit_L:
     if(alert_name.compare("P0_ALERT")   == 0 ) {
         scheduleP0AlertEventHandler();
     }
@@ -382,7 +373,7 @@ bool harvest_ras_errors(struct i2c_info info,std::string alert_name)
         scheduleP1AlertEventHandler();
     }
 
-    return true;
+    return status;
 }
 
 int main() {
