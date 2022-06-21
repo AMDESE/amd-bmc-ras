cac88fd5a2fec624c27e786b3a280dfdbd97ec27
Abinaya <abinaya.dhandapani@amd.com>
2022-06-21 05:06:16 -0500

    Create single CPER file for 2P systems
    1. Create single CPER record for 2P systems.
    2. Handle system recovery for MP1 errors. A SEL event will be logged if an error
       is detected in the control fabric.
    3. Set default config policy to "No reset".
    4. Maximum error files allowed is 10. Rotate the error file index if it exceeds 10.
    5. Remove raw file support.
    
    Signed-off-by: Abinaya <abinaya.dhandapani@amd.com>

--- inc/cper.hpp
+++ inc/cper.hpp
@@ -4,15 +4,17 @@
 #define CPER_SIG_SIZE               (4)
 #define CPER_SIG_RECORD             ("CPER")
 #define CPER_RECORD_REV             (0x0100)
+#define SECTION_COUNT               (2)
 #define CPER_SIG_END                (0xffffffff)
 #define CPER_SEV_FATAL              (1)
-#define CTX_TYPE_MSR                (0x02)
+#define CTX_OOB_CRASH               (0x01)
 #define CPER_PRIMARY                (0)
 #define RSVD                        (0)
-#define TBD                         (0)
 #define GENOA_MCA_BANKS             (32)
 #define MCA_BANK_MAX_OFFSET         (128)
-
+#define SYS_MGMT_CTRL_ERR           (0x04)
+#define DF_DUMP_RESERVED            (6143)
+#define MAX_ERROR_FILE              (10)
 /*
  * CPER section header revision, used in revision field in struct
  * cper_section_descriptor
@@ -30,7 +32,7 @@
 
 #define CPU_ID_VALID                    (0x02)
 #define LOCAL_APIC_ID_VALID             (0x01)
-
+#define PROC_CONTEXT_STRUCT_VALID       (0x100)
 #define INFO_VALID_CHECK_INFO           (0x01)
 
 #define FRU_ID_VALID                    (0x01)
@@ -55,11 +57,8 @@ typedef struct {
     GUID_INIT(0x61fa3fac, 0xcb80, 0x4292, 0x8b, 0xfb, 0xd6, 0x43,   \
           0xb1, 0xde, 0x17, 0xf4)
 #define VENDOR_OOB_CRASHDUMP                    \
-    GUID_INIT(0xbe44d8de, 0xe33b, 0x49f7, 0xb7, 0x0a, 0x1b, 0x07,   \
-          0x07, 0x7e, 0x2e, 0xb4)
-#define AMD_ERR_STRUCT_TYPE                     \
-    GUID_INIT(0xcda04b87, 0x16c8, 0x45c8, 0x8d, 0x2d, 0x08, 0x02,   \
-          0x15, 0xb1, 0x0c, 0xe8)
+    GUID_INIT(0x32AC0C78, 0x2623, 0x48F6, 0xB0, 0xD0, 0x73, 0x65,   \
+          0x72, 0x5F, 0xD6, 0xAE)
 
 typedef struct {
   uint32_t mca_data[MCA_BANK_MAX_OFFSET];
@@ -137,6 +136,7 @@ typedef struct proc_info PROCINFO;
 struct df_dump {
   uint32_t                           dfwdtdump_high;
   uint32_t                           dfwdtdump_low;
+  uint64_t                           reserved[DF_DUMP_RESERVED];
 }  __attribute__((packed));
 
 typedef struct df_dump DF_DUMP;
@@ -153,13 +153,20 @@ struct context_info {
 typedef struct context_info CONTEXT_INFO;
 
 struct error_record {
-    COMMON_ERROR_RECORD_HEADER        Header;
-    ERROR_SECTION_DESCRIPTOR          SectionDescriptor;
     PROCESSOR_ERROR_SECTION           ProcError;
     PROCINFO                          ProcessorInfo;
     CONTEXT_INFO                      ContextInfo;
-}  __attribute__((packed));
+} __attribute__((packed));
 
 typedef struct error_record ERROR_RECORD;
+
+struct cper_record {
+    COMMON_ERROR_RECORD_HEADER        Header;
+    ERROR_SECTION_DESCRIPTOR          SectionDescriptor[2];
+    ERROR_RECORD                      P0_ErrorRecord;
+    ERROR_RECORD                      P1_ErrorRecord;
+}  __attribute__((packed));
+
+typedef struct cper_record CPER_RECORD;
 
 #endif
