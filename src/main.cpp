645ebafbd21a1df060fffe23c62eb1568950eb04
Aashray Boddu <Aashray.Boddu@amd.com>
2026-01-14 23:03:27 -0600

    Revert "Integrate APML Alert_L APIs to monitor RAS alerts"
    This reverts commit 21d3a061170bc34b0323f5b4c9889c50783e809f.

--- src/main.cpp
+++ src/main.cpp
@@ -55,13 +55,6 @@ int main(int argc, char* argv[])
 #endif
 
     io.run();
-#ifdef APML
-    auto* apmlMgr = dynamic_cast<amd::ras::apml::Manager*>(errorMgr);
-    if (apmlMgr && apmlMgr->getAlertHandleMode() == "UEVENT")
-    {
-        apmlMgr->releaseUdevReSrc();
-    }
-#endif
 
     return 0;
 }
