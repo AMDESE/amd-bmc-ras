1058ebab77ce0979e6643ff70c04530b9e90645f
khashaik <105347289+khashaik@users.noreply.github.com>
2026-05-27 08:39:54 +0530

    Fix issue in RAS APP to generate the ras cper file for Node-2 in 2 X … (#166)
    * Fix issue in RAS APP to generate the ras cper file for Node-2 in 2 X 1P mode
    
     - In 2X1P Mode: RAS APP  Node-2 was not generating the RAS CPER file on fatal alert.
       Fixed this issue and verified on Congo and Morocco platforms
    
    Signed-off-by: Raja Sekhar Reddy Gade <RajaSekharReddy.Gade@amd.com>

--- src/apml_manager.cpp
+++ src/apml_manager.cpp
@@ -36,6 +36,8 @@ constexpr size_t shutdownError = 0x40;
 constexpr size_t socket0 = 0;
 constexpr size_t socket1 = 1;
 
+constexpr auto secondNodeId = "2";
+
 constexpr uint32_t badData = 0xBAADDA7A;
 constexpr size_t fatalError = 1;
 constexpr size_t resetHangErr = 0x2;
@@ -584,7 +586,7 @@ void Manager::registerEventHandler()
             requestGPIOEvents(socketNames[i],
                               std::bind(&ras::apml::Manager::alertEventHandler,
                                         this, std::ref(gpioEventDescriptors[i]),
-                                        std::ref(gpioLines[i]), i),
+                                        std::ref(gpioLines[i]), socIndex[i]),
                               gpioLines[i], gpioEventDescriptors[i]);
         }
     }
@@ -674,7 +676,7 @@ void Manager::clearSbrmiAlertMask(uint8_t socNum)
 void Manager::alertSrcHandler(struct apml_udev_monitor* udev_mon,
                               uint8_t socket)
 {
-    uint8_t soc_num = 0;
+    uint8_t soc_num = (node == secondNodeId) ? socket1 : socket0;
     uint32_t src = 0;
     bool block = false;
     oob_status_t ret;
@@ -2225,7 +2227,14 @@ bool Manager::decodeInterrupt(uint8_t socNum, uint32_t src)
     }
     configMgr.updateErrorCountDbus();
     configMgr.saveErrorCounts();
-    cpuAlertProcessed[socNum] = true;
+
+    // Map physical socNum to logical index in cpuAlertProcessed
+    auto socIt = std::find(socIndex.begin(), socIndex.end(),
+                           static_cast<size_t>(socNum));
+    if (socIt != socIndex.end())
+    {
+        cpuAlertProcessed[std::distance(socIndex.begin(), socIt)] = true;
+    }
 
     if (fchHangError == true || runtimeError == true ||
         nonMcaShutdownError == true)
@@ -2599,7 +2608,15 @@ bool Manager::decodeInterrupt(uint8_t socNum)
             }
             configMgr.updateErrorCountDbus();
             configMgr.saveErrorCounts();
-            cpuAlertProcessed[socNum] = true;
+
+            // Map physical socNum to logical index in cpuAlertProcessed
+            auto socIt = std::find(socIndex.begin(), socIndex.end(),
+                                   static_cast<size_t>(socNum));
+            if (socIt != socIndex.end())
+            {
+                cpuAlertProcessed[std::distance(socIndex.begin(), socIt)] =
+                    true;
+            }
 
             // Clear RAS status register
             // 0x4c is a SB-RMI register acting as write to clear
