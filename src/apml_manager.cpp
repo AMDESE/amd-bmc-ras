3c422d80d584c789db696222dfebb128f8e41869
aasaitha <aasaitha@amd.com>
2026-06-11 13:27:02 +0000

    Always apply DRAM CECC leak-rate config regardless of polling setting.
    Signed-off-by: aasaitha <aasaitha@amd.com>

--- src/apml_manager.cpp
+++ src/apml_manager.cpp
@@ -2407,6 +2407,24 @@ oob_status_t Manager::setMcaOobConfig()
         oob_config.dram_cecc_leak_rate =
             static_cast<uint8_t>(*dramCeccLeakRate);
     }
+    else
+    {
+        amd::ras::config::Manager::AttributeValue dramCeccOobEcModeVal =
+            configMgr.getAttribute("DramCeccOobEcMode");
+        int64_t* dramCeccOobEcMode =
+            std::get_if<int64_t>(&dramCeccOobEcModeVal);
+
+        amd::ras::config::Manager::AttributeValue dramCeccLeakRateVal =
+            configMgr.getAttribute("DramCeccLeakRate");
+        int64_t* dramCeccLeakRate =
+            std::get_if<int64_t>(&dramCeccLeakRateVal);
+
+        oob_config.dram_cecc_oob_ec_mode =
+            static_cast<uint8_t>(*dramCeccOobEcMode);
+        oob_config.dram_cecc_leak_rate =
+            static_cast<uint8_t>(*dramCeccLeakRate);
+        oob_config.mca_oob_misc0_ec_enable = 1;
+    }
 
     ret = setRasOobConfig(oob_config);
 
