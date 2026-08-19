053b7c3add84506fe37f63c313731df7e4c41397
Raja Sekhar Reddy Gade <RajaSekharReddy.Gade@amd.com>
2026-08-19 00:26:46 -0500

    bump CPER minor revision for RAS changes in v100
    Update the CPER minor revision from 0x0D to 0x0E for RAS changes in
    OpenBMC Venice v2.18.0.100 release
    
    Signed-off-by: Raja Sekhar Reddy Gade <RajaSekharReddy.Gade@amd.com>

--- include/utils/cper.hpp
+++ include/utils/cper.hpp
@@ -37,7 +37,7 @@ constexpr uint8_t cperValidTimestamp = 0x2;
 constexpr uint8_t addcGenNumber3 = 0x03;
 constexpr uint8_t familyId1ah = 0x1A;
 constexpr uint16_t pcieVendorId = 0x1022;
-constexpr uint8_t minorRevision = 0x0D;
+constexpr uint8_t minorRevision = 0x0E;
 
 /** @brief Finds a filename in the RAS directory that matches a given pattern.
  *
